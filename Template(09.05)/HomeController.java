b2b8639cf083d124b5cc400d1e5d410ff682aca5
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2022-05-09 12:08:49 -0300

    Add files via upload

---
+++ Template(09.05)/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index () {
+		return "index";
+	}
+	
+	@GetMapping("/signin")
+	public String signin() {
+		return "signin";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(Model model) {
+		model.addAttribute("nome", "Lucas Daniel");
+		model.addAttribute("descricao", "Football");
+		return "dashboard";
+	}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
+}
